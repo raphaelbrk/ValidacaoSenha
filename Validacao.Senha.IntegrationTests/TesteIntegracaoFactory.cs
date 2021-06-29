3c7ec6e5929e4da84036de51996563b405cdbcef
Raphael do Rêgo Guimarães <raphaelguimaraes4@gmail.com>
2021-06-29 11:47:52 -0300

    - Exclusão do teste integração factory.

--- Validacao.Senha.IntegrationTests/TesteIntegracaoFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-using Validacao.Senha.Web;
-
-namespace Validacao.Senha.IntegrationTests
-{
-    public class TesteIntegracaoFactory : WebApplicationFactory<Startup>
-    {
-    }
-}
